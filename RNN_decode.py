e994a2ec351911ddc77402c64c7e4fdc0d254cd4
OJL <1791088334@qq.com>
2022-07-13 20:45:00 +0800

    解码代码更新

--- RNN_decode.py
+++ RNN_decode.py
@@ -1,6 +1,8 @@
 import torch
+import torch.nn as nn
 import numpy as np
 from RNN_model import M_RNN
+from BPE_handle.word22id import word2id, id2word, get_word, replace_word
 
 PATH_MODEL = "./RNN_train_state/train_state_73000.pth"
 PATH_INDEX = "F:/PyTorch学习/BPE_handle/en_index.npy"
@@ -15,12 +17,17 @@ def load(srcf):
 if __name__ == '__main__':
     en2index = load(PATH_INDEX)
     # print(en2index)
+    en = get_word(en2index)
+    # print(en)
 
     sentence = input("请输入测试语句:").strip().split()
+    # print(sentence)
+
+    sentence = replace_word(sentence, en)
     print(sentence)
 
-    word2id = [en2index[i] for i in sentence]
-    print(word2id)
+    word2id_l = word2id(en2index, sentence)  # !!!
+    # print(word2id)
 
     model = M_RNN(len(en2index))
 
@@ -39,18 +46,29 @@ if __name__ == '__main__':
     # net_state_dict.update(pretrain_dict_1)
     # model.load_state_dict(net_state_dict)
 
-    predict_step = len(word2id)
-    input = []
+    decode_step = len(sentence)
+    predict_step = len(word2id_l)
+
+    state = None
+    # print(state.is_cuda)
+    result = []
+
+    # 先将整句话过一遍decode
+    for i in range(decode_step):
+        words = torch.LongTensor([word2id_l[i]]).to(device)
+        output, state = model.decode(words, state)
+        # print(state)
+        # print("**"*40)
+
+    # print(state)
 
     for i in range(predict_step):
-        words = torch.LongTensor([word2id[i]]).to(device)  # 每次取一个词的索引
-        output = model.decode(words)  # 解码获得最可能的下一个词
-        input.append(output.item())
+        # print(state)
+        words = torch.LongTensor([word2id_l[i]]).to(device)  # 每次取一个词的索引
+        output, state = model.decode(words, state)  # 解码获得最可能的下一个词
+        result.append(output.item())
 
-    result_words = []
+    result_words = id2word(en2index, result)
+    print(result_words)
 
-    for i in range(len(input)):
-        s = [en for en, index in en2index.items() if index == input[i]]
-        result_words.append("".join(s))
-
-    print(" ".join(result_words).replace("@@ ", ""))
+    print("预测句子结果为:{}".format(" ".join(result_words).replace("@@ ", "")))
